c6872ca0871ff56c938954912dc29220fc43a749
AbramBuck <82072155+AbramBuck@users.noreply.github.com>
2024-06-18 22:13:25 -0500

    Project Comleted

--- aa01-problem-solving/problems/04-commenting-code.js
+++ aa01-problem-solving/problems/04-commenting-code.js
@@ -7,7 +7,7 @@ in the code so that the example input matches the expected output.
 
 function charactersNotQuiteAtTheEdge(str) {
   let char1 = str[1];
-  let char2 = str[str.length - 1];
+  let char2 = str[str.length - 2];
   return char1 + char2;
 }
 
